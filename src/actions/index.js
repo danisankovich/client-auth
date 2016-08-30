42da482b5690a68861c2236840678c869390fbaf
Michael Sankovich <misankovich@gmail.com>
2016-08-30 00:26:58 -0400

    error on bad login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router'; // commits info about url to react router, and to make changes to url
+import {AUTH_USER, AUTH_ERROR} from './types';
 
 const ROOT_URL = 'http://localhost:3000';
 
@@ -7,6 +9,24 @@ export function signinUser({email, password}) {
                               //function. it lets use return a function instead
                               //of an object from action creator
       axios.post(`${ROOT_URL}/signin`, { email, password })
-      // dispatch({ type: ... });
+        .then(response => {
+          dispatch({type: AUTH_USER});
+
+          localStorage.setItem('token', response.data.token);
+
+          browserHistory.push('/information'); // success pushes you to /information.
+
+        })
+        .catch(() => {
+          // catch does not take you to new page
+          dispatch(authError('EMAIL/PASSWORD combo incorrect'));
+        })
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -4,8 +4,16 @@ import * as actions from '../../actions';
 
 class Signin extends Component {
   handleFormSubmit({email, password}) {
-    console.log(email, password);
     this.props.signinUser({email, password});
+  }
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
   }
   render() {
     const { handleSubmit, fields: {email, password}} = this.props;
@@ -19,13 +27,18 @@ class Signin extends Component {
           <label>Password: </label>
           <input {...password} className="form-control"/>
         </fieldset>
+        { this.renderAlert() }
         <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
     )
   }
 }
 
-export default reduxForm({
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
+export default reduxForm({ // basically same as connect
   form: 'signin',
   fields: ['email', 'password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
