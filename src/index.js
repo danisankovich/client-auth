d7c1672e6cef96fb89a5f1386f2150771582faf5
Michael Sankovich <misankovich@gmail.com>
2016-08-31 22:40:50 -0400

    signup validation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
 import Signout from './components/auth/signout';
 
 import reducers from './reducers';
@@ -18,6 +19,7 @@ ReactDOM.render(
     <Router history={browserHistory}>
         <Route path='/' component={App}>
           <Route path='signin' component={Signin}></Route>
+          <Route path='signup' component={Signup}></Route>
           <Route path='signout' component={Signout}></Route>
         </Route>
     </Router>
